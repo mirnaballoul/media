7b04d9694f0c16ee144ecc0f607194764ceff420
mirna <mirnaballoul3@gmail.com>
2021-04-15 15:33:08 -0400

    first commit

--- main.js
+++ main.js
@@ -6,12 +6,20 @@ logIn.addEventListener('click',  logInUser);
 function logInUser() {
     let e=document.getElementById("email").value;
     let p=document.getElementById("password").value;
-    fetch('http://localhost:3000/users').then(response => response.json()).then(data => {
+    fetch('http://localhost:3000/users').then(response => response.json()).then(async data => {
 
         for(let i =0 ; i < data.length; ++i)
         {
             if(data[i].email==e && data[i].passowrd==p){
-                    window.location.assign('file:///C:/Users/Shaam/Desktop/JSON/social/posts.html?');
+                const res = await axios.put('http://localhost:3000/active/1' , {
+                        passowrd:data[i].password,
+                        name :data[i].name,
+                        image:data[i].image,
+                        email: data[i].email
+                    }).then(() => {
+
+                        window.location.assign('file:///C:/Users/Shaam/Desktop/JSON/social/posts.html?');
+                    })
             }
         }
         let err= document.getElementById("password_error").innerText = "ERROR IN PASSWORD";
--- pos.js
+++ pos.js
@@ -1,77 +1,93 @@
 let posts = [];
-let comments=[];
+let comments = [];
+
 window.addEventListener('load', () => {
-   
+
     posts = fetch('http://localhost:3000/posts')
-    .then(posts => posts.json()).then( posts =>
-        posts.forEach(element => {
-            
+        .then(posts => posts.json()).then(posts =>
+            posts.forEach(element => {
 
-            let i=document.createElement('i');
-            i.setAttribute('class', 'glyphicon glyphicon-thumbs-up icon');
-            
-            let publisher = document.createElement('h4');
-            publisher.innerText = element.publisher;
-            publisher.setAttribute('class', 'titleClass');
-            
-            let content  = document.createElement('h3');
-            content.setAttribute('style', 'float:left; margin-left:50px');
-            content.innerText = element.content;
+                let publisher = document.createElement('h4');
+                publisher.innerText = element.publisher;
+                publisher.setAttribute('class', 'titleClass');
 
-            let reactions=document.createElement('h4');
-            reactions.innerText=element.reactions;
-            reactions.setAttribute('class', 'reactclass');
+                let content = document.createElement('h3');
+                content.setAttribute('style', 'float:left; margin-left:50px');
+                content.innerText = element.content;
 
-            let img=document.createElement('img');
-            img.setAttribute('src', './image/5.png');
-            img.setAttribute('class', 'reactclass imgReactClass');
+                let reactions = document.createElement('h4');
+                reactions.innerText = element.reactions;
+                reactions.setAttribute('class', 'reactclass');
 
-            let image = document.createElement('img');
-            image.src=element.image;
-            image.setAttribute('class', 'imageClass');
-            let dv = document.createElement('div');
-            dv.setAttribute('class', 'divClass'); 
-            dv.appendChild(reactions);
-            dv.appendChild(i);
-            dv.appendChild(image);
-            dv.appendChild(publisher);
-            dv.appendChild(content);
-            dv.appendChild(img);
+                let img = document.createElement('img');
+                img.setAttribute('src', './image/5.png');
+                img.setAttribute('class', 'reactclass imgReactClass');
 
-            
-            
-            
-            let commentsDiv = document.createElement('div');
-            for (let i = 0; i <element.comments.length; i++)
+                let image = document.createElement('img');
+                image.src = element.image;
+                image.setAttribute('class', 'imageClass');
+                let dv = document.createElement('div');
+                dv.setAttribute('class', 'divClass');
+                dv.appendChild(reactions);
+                dv.appendChild(image);
+                dv.appendChild(publisher);
+                dv.appendChild(content);
+                dv.appendChild(img);
 
-            {
-                let dvv = document.createElement('div');
-                dvv.setAttribute('class', 'divvClass');
-                
-                let publisherr = document.createElement('h4');
-                publisherr.innerText = element.comments[i].publisher;
-                publisherr.setAttribute('class', 'titleeClass');
-                
-                let imagee = document.createElement('img');
-                imagee.src=element.comments[i].image;
-                imagee.setAttribute('class', 'imageeClass');
 
-                let contentt  = document.createElement('h3');
-                contentt.innerText = element.comments[i].content;
 
-                dvv.appendChild(imagee);
-                dvv.appendChild(publisherr);
-                dvv.appendChild(contentt);
 
-                commentsDiv.appendChild(dvv);
-            }
-            commentsDiv.setAttribute('style', 'margin-top:40px');
-            let postsDialog = document.getElementById('posts');
-            postsDialog.setAttribute('class', 'divClass'); 
-            dv.appendChild(commentsDiv);
-            postsDialog.appendChild(dv);
-        }));
+                let commentsDiv = document.createElement('div');
+                for (let i = 0; i < element.comments.length; i++) {
+                    let dvv = document.createElement('div');
+                    dvv.setAttribute('class', 'divvClass');
+
+                    let publisherr = document.createElement('h4');
+                    publisherr.innerText = element.comments[i].publisher;
+                    publisherr.setAttribute('class', 'titleeClass');
+
+                    let imagee = document.createElement('img');
+                    imagee.src = element.comments[i].image;
+                    imagee.setAttribute('class', 'imageeClass');
+
+                    let contentt = document.createElement('h3');
+                    contentt.innerText = element.comments[i].content;
+
+                    dvv.appendChild(imagee);
+                    dvv.appendChild(publisherr);
+                    dvv.appendChild(contentt);
+
+                    commentsDiv.appendChild(dvv);
+                }
+                commentsDiv.setAttribute('style', 'margin-top:40px');
+                let postsDialog = document.getElementById('posts');
+                postsDialog.setAttribute('class', 'divClass');
+                dv.appendChild(commentsDiv);
+                postsDialog.appendChild(dv);
+            }));
+
+
 });
 
+let id = 1;
 
+function addNewPost() {
+    let current = fetch('http://localhost:3000/active')
+        .then(active => active.json()).then(async current => {
+            console.log(current);
+            let con = document.getElementById('post').value;
+            let posts = await axios.get('http://localhost:3000/posts');
+            axios.post('http://localhost:3000/posts', {
+                "id": posts.length + 1,
+                "publisher": current[0].name,
+                "content": con,
+                "reactions": "0",
+                "image": current[0].image,
+                "comments": [],
+            })
+                .then((response) => {
+                    window.location.assign('file:///C:/Users/Shaam/Desktop/JSON/social/posts.html');
+                })
 
+        })
+}
